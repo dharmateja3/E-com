c38ce00f642942cec6f266887553f1aa9d1ae212
Dussa Dharma Teja <dharmatejadussa@gmail.com>
2023-07-07 12:57:34 +0530

    ok

--- LoginScript.js
+++ LoginScript.js
@@ -1,5 +1,21 @@
-const btn = document.getElementById('btn');
-function login(e){
-    e.preventDefault();
+const login=(event)=>{
+    event.preventDefault()
+    const email = document.getElementById(`myemail`).value
+    const password = document.getElementById(`mypwd`).value
+    console.log(email,password)
+   
+if(email===`dharmatejadussa@gmail.com` && password===`teja@123`){
+    alert(`Login Successfull!!`)
+    localStorage.setItem(`auth`,true)
+    window.location.href="index.html"
 }
-btn.addEventListener(`click`,login(e));
+else{
+    alert(`Email or Password is incorrect!`)
+}
+}
+const verify=()=>{
+    if(localStorage.auth){
+        window.location.href="/"
+    }
+}
+verify()
--- script.js
+++ script.js
@@ -1,5 +1,37 @@
+const FetchData = async()=>{
+    const res = await fetch("https://dummyjson.com/products");
+    const data = await res.json();
+    for (let i =0 ; i<data.products.length; i++){
+        const myCont = document.getElementById("mymain");
+        const card = document.createElement("div");
+        const image = document.createElement("img");
+        const title = document.createElement("span");
+        const price = document.createElement("span");
 
+        card.classList.add("card");
+        image.classList.add("image");
+        title.classList.add(`drop`);
+        price.classList.add(`drop`);
+    
+        image.src=data.products[i].thumbnail;
+        title.innerText=data.products[i].brand;
+        price.innerText=data.products[i].price;
+
+        card.append(image,title,price);
+        myCont.append(card);
+    } 
+};
+
+const verify=()=>{
+    if(localStorage.auth){
+        FetchData();
+    }
+    else{
+        window.location.href="/login.html";
+    }
+};
 const logout=()=>{
+    alert(`Logged Out Successfully!!`);
+    localStorage.removeItem(`auth`);
     window.location.href="/login.html"
-    // setTimeout({alert(`Logged Out Successfully`);},2000);
-}
+};
